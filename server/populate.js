69f8294584e0d6e64c8309d6d0e59800f7d7e295
nickolanick <bilayev@ucu.edu.ua>
2019-06-10 13:23:49 +0300

    Setup Npm and remte db

--- server/populate.js
+++ server/populate.js
@@ -1,51 +1,46 @@
 const faker = require('faker');
 
-
 const Course = require('./db').Course;
 const Lesson = require('./db').Lesson;
 const User = require('./db').User;
-const users = [];
 
+const generateRandomUsers = async (amount) =>
+    await Promise.all(new Array(amount).fill(0).map(async () =>
+        await ((new User({
+            "name": faker.name.findName(),
+            "password": faker.lorem.word(),
+            "email": faker.internet.email()
+        })).save())
+    ));
 
-for (let i = 0; i < 10; i++) {
-  users.push({
-    name: faker.name.findName(),
-    password: 'pass',
-    email: faker.internet.email(),
-  });
-}
-User.find({}, (err, user) => {
-  let author_id = user[0].id;
+const generateRandomCourse = async (amount, author_id) =>
+    await Promise.all(new Array(amount).fill(0).map(async () =>
+        await ((new Course({
+            courseName: faker.lorem.words(),
+            courseTech: faker.lorem.words(),
+            courseDescription: faker.lorem.sentence(),
+            lessons: [],
+            author: author_id,
+        })).save())
+    ));
 
-  for (let i = 0; i < 5; i++) {
-    Course.insertMany(
-      {
-        courseName: faker.lorem.words(),
-        courseTech: faker.lorem.words(),
-        courseDescription: faker.lorem.sentence(),
-        lessons: [],
-        author: author_id,
-      },
-      (err, res) => {
-        console.log(err, res);
-      },
-    );
-    for (let i = 0; i < 5; i++) {
-      let new_lesson = new Lesson({
-        name: faker.lorem.words(),
-        language: faker.lorem.words(),
-        time: '1:14',
-        video: 'test',
-        author: author_id,
-      });
-      new_lesson.save();
-      Course.find({ author: author_id }, (err, courses) => {
-        courses.map(course => {
-          course.lessons.push(new_lesson);
-          course.save();
-        });
-      });
-    }
-  }
-});
-process.exit();
+const generateRandomLessons = async (amount, author_id) =>
+    await Promise.all(new Array(amount).fill(0).map(async () =>
+        await ((new Lesson({
+            name: faker.lorem.words(),
+            language: faker.lorem.words(),
+            time: '1:14',
+            video: 'test',
+            author: author_id,
+        })).save())
+    ));
+
+const generateData = async () => {
+    let users_amount = 4;
+    let courses_per_user = 3;
+    let lessons_per_user = 3;
+    const users = await generateRandomUsers(users_amount);
+    const random_courses = await Promise.all(users.map(async (user) => await generateRandomCourse(courses_per_user, user["_id"])));
+    const random_lessons = await Promise.all(users.map(async (user) => await generateRandomLessons(lessons_per_user, user["_id"])));
+};
+generateData();
